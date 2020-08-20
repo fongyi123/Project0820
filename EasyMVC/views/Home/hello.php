ec102f24cc8df2ab7f6988dc0dca8b7499b600d4
fong-yi <you@example.com>
2020-08-20 19:22:19 +0800

    0820上課筆記與作業雛形

---
+++ EasyMVC/views/Home/hello.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <title>Lab</title>
+</head>
+<body>
+    <H1>Hello! <?= $data->name ?></H1>
+    <!-- 輸出data的name -->
+</body>
+</html>
