179a6b20f0c55afba1b95da288f74ee1ab4e8bb7
fong-yi <fong-yi@mail.chungyo.net>
2020-08-22 14:43:13 +0800

    0822作業完成

--- 0817/Lab_Login/Lab_Login/index.php
+++ 0817/Lab_Login/Lab_Login/index.php
@@ -1,47 +1,49 @@
 <?php 
 session_start();
-// if (isset($_SESSION["uid"])){
-//   $userName = $_SESSION["uid"];
-// }  
-// else{
-//   $userName = "Guest";
-// }
-// $userName = "Guest";
-// echo "OK";
 if (isset($_SESSION["uid"])){
   $userName = $_SESSION["uid"];
 }  
 else{
   $userName = "Guest";
 }
-  
 
+// if (isset($_COOKIE["uid"])){
+//   $userName = $_COOKIE["uid"];
+// }  
+// else{
+//   $userName = "Guest";
+// } 
 
 ?>
-
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
+<style>
+body {
+  background-color: lightblue;
+}
+
+</style>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Lab - index</title>
 </head>
 <body>
 
-<table width="300" border="0" align="center" cellpadding="5" cellspacing="0" bgcolor="#F2F2F2">
+<table width="500" height="200" border="0" align="center" cellpadding="5" cellspacing="0" bgcolor="#F2F2F2" style="background-color:Violet;">
   <tr>
-    <td align="center" bgcolor="#CCCCCC"><font color="#FFFFFF">會員系統 - 首頁</font></td>
+    <td align="center" bgcolor="#CCCCCC" style="background-color:SlateBlue;"><font color="#FFFFFF" >會員系統 - 首頁</font></td>
   </tr>
   <tr>
   <?php if ($userName == "Guest") { ?>
-    <td align="center" valign="baseline"><a href="login.php">登入</a> |
+    
+    <td align="center"><font color="#FFFFFF"><a href="login.php"><font color="#FFFFFF">登入</a> |
     <?php } else { ?>
-    <td align="center" valign="baseline"><a href="login.php?logout=1">登出</a> | 
+    <td align="center"><a href="login.php?logout=1"><font color="#FFFFFF">登出</a> | 
     <?php } ?>   
-    <a href="secret.php">會員專用頁</a></td>
+    <a href="secret.php"><font color="#FFFFFF">會員專用頁</a></td>
   </tr>
   <tr>
-    <td align="center" bgcolor="#CCCCCC"><?= "Welcome! " .$userName ?></td>
+    <td align="center" bgcolor="#CCCCCC" style="background-color:SlateBlue;"><font color="#FFFFFF" ><?= "Welcome! " .$userName ?></td>
   </tr>
 </table>
 
--- 0817/Lab_Login/Lab_Login/login.php
+++ 0817/Lab_Login/Lab_Login/login.php
@@ -1,33 +1,35 @@
 <?php
 session_start();
-// echo $_GET["logout"];
 if( isset( $_GET["logout"])){
+
+  // setcookie("uid", "Guest",time() -60 * 60 * 24 * 7);
   $_SESSION["uid"]=$userName;
   echo $_SESSION["logout"];
   unset($_SESSION["logout"]);
-  // https://docs.w3cub.com/php/function.session-status/
-  // setcookie("uid", "Guest",time() -60 * 60 * 24 * 7);
+
   header("Location: index.php");
   exit();
 }
-if (isset($_POST["btnHome"]))
-{
+if (isset($_POST["btnHome"])){
 	header("Location: index.php");
 	exit();
 }
 
 if (isset($_POST["btnOK"])){
   $userName = $_POST["txtUserName"];
-  if (trim($userName) != "")
-  
-	{
+  if (trim($userName) != ""){
+
     // setcookie("uid", $userName);
     $_SESSION["uid"]=$userName;
     echo $_SESSION["lastPage"];
     unset($_SESSION["lastPage"]);
+
     // 網頁紀錄憑證
+    // if (isset($_COOKIE["lastPage"]))
+    //   header(sprintf("Location: %s", $_COOKIE["lastPage"]));
     if (isset($_SESSION["lastPage"]))
-		  header(sprintf("Location: %s", $_SESSION["lastPage"]));
+      header(sprintf("Location: %s", $_SESSION["lastPage"]));
+      
 		else
 		   header("Location: index.php");
     exit();
@@ -37,25 +39,32 @@ if (isset($_POST["btnOK"])){
 
 <html>
 <head>
+<style>
+body {
+  background-color: lightblue;
+}
+
+</style>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Lab - Login</title>
 </head>
 <body>
-<form id="form1" name="form1" method="post" action="login.php">
-  <table width="300" border="0" align="center" cellpadding="5" cellspacing="0" bgcolor="#F2F2F2">
+<form id="form1" name="form1" method="post" action="login.php" >
+  <table width="500" height="200" border="0" align="center" cellpadding="5" cellspacing="0" bgcolor="#F2F2F2" style="background-color:Violet;">
     <tr>
-      <td colspan="2" align="center" bgcolor="#CCCCCC"><font color="#FFFFFF">會員系統 - 登入</font></td>
+      <td colspan="2" align="center" bgcolor="#CCCCCC" style="background-color:SlateBlue;"><font color="#FFFFFF">會員系統 - 登入</font></td>
     </tr>
     <tr>
-      <td width="80" align="center" valign="baseline">帳號</td>
-      <td valign="baseline"><input type="text" name="txtUserName" id="txtUserName" /></td>
+      <td width="80" align="center" ><font color="#FFFFFF">帳號 :</td>
+      <td><input type="text" name="txtUserName" id="txtUserName" pattern="\w{7,14}" required/></td>
     </tr>
     <tr>
-      <td width="80" align="center" valign="baseline">密碼</td>
-      <td valign="baseline"><input type="password" name="txtPassword" id="txtPassword" /></td>
+      <td width="80" align="center" valign="baseline"><font color="#FFFFFF">密碼 :</td>
+      <td valign="baseline"><input type="password" name="txtPassword" id="txtPassword" pattern="\w{7,14}" required/></td>
     </tr>
     <tr>
-      <td colspan="2" align="center" bgcolor="#CCCCCC"><input type="submit" name="btnOK" id="btnOK" value="登入" />
+      <td colspan="2" align="center" bgcolor="#CCCCCC" style="background-color:SlateBlue;"><font color="#FFFFFF" >
+      <input type="submit" name="btnOK" id="btnOK" value="登入" />
       <input type="reset" name="btnReset" id="btnReset" value="重設" />
       <input type="submit" name="btnHome" id="btnHome" value="回首頁" />
       </td>
